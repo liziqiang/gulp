f5b393193de6022a9da92f493cba9782b0132d5e
lizq <ziqiang.lee@gmail.com>
2016-10-09 22:18:43 +0800

    pack Javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@ var gulp = require( 'gulp' ),
     pump = require( 'pump' ),
     $    = require( 'gulp-load-plugins' )();
 // parsing less files
-gulp.task( 'ParsingLess', function( cb ) {
+gulp.task( 'ParseLess', function( cb ) {
     pump( [
         gulp.src( 'mview/**/*.less' ),
         $.less(),
         gulp.dest( 'src/static/css' )
     ], cb );
 } );
-gulp.task( 'watch', function() {
-    gulp.watch( 'mview/**/*.less', [ 'ParsingLess' ] );
+// packing javascript files
+gulp.task( 'PackJavascript', function( cb ) {
+    pump( [
+        gulp.src( 'mview/**/*.js' ),
+        $.uglify(),
+        gulp.dest( 'src/static/js' )
+    ], cb );
 } );
-gulp.task( 'default', [ 'ParsingLess', 'watch' ] );
+gulp.task( 'watch', function() {
+    gulp.watch( 'mview/**/*.less', [ 'ParseLess' ] );
+} );
+gulp.task( 'default', [ 'ParseLess', 'PackJavascript', 'watch' ] );
