e413377068c6316bb635be74c206b9fcd5c5a4f9
lizq <ziqiang.lee@gmail.com>
2016-10-11 10:18:22 +0800

    modify

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,16 @@
 /*! functions.js */
-var $ = require('jquery');
-$.fn.slider = require('ui/slider.js');
-
-$(window).load(function() {
-  var slider = $('.banner').slider({
-    dots: true,
-    fluid: true,
-    arrows: true
-  });
-  $('.slider-arrow').click(function() {
-    var fn = this.className.split(' ')[1];
-
-    //  Either do slider.data('slider').next() or .prev() depending on the className
-    slider.data('slider')[fn]();
-  });
-});
+var $       = require( 'jquery' );
+$.fn.slider = require( 'ui/slider.js' );
+$( window ).load( function() {
+    var slider = $( '.banner' ).slider( {
+        dots   : true,
+        fluid  : true,
+        arrows : true
+    } );
+    $( '.slider-arrow' ).click( function() {
+        var fn = this.className.split( ' ' )[ 1 ];
+        slider.data( 'slider' )[ fn ]();
+    } );
+} );
 
 
